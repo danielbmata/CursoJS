6f32695d8d384f0b0fba99a8472604a184ca6679
Daniel <d7mata@gmail.com>
2023-11-09 09:04:26 -0300

    constantes

--- aula6_variaveis/index.js
+++ aula6_variaveis/index.js
@@ -11,3 +11,30 @@ console.log('O filho de', nome, 'se chama Eduardo');
 let nome2;  // Declarei a var
 nome2 = 'Qualquer valor'  // Iniciei a var
 console.log(nome2)
+
+// Mudando o valor da var
+
+nome2 = 123 
+console.log(nome2)
+
+/*
+  Não posso re-declarar uma var usando let, geraria erros.
+let nome = 'Daniel'
+*/
+
+/*
+  Não podemos criar variaveis com palavras reservadas
+let let 
+let console
+let if
+Etc...
+*/
+
+/*
+  Não podemos começar o nome de uma var com um numero
+  e não pode conter espaços ou traços
+let 01nome = 'Daniel'
+*/
+
+// Boa pratica é utilizar camelCase
+let nomeCompletoDoCliente;
---
+++ aula7_constantes/index.js
@@ -0,0 +1,26 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+
+// +  -  *  /
+const primeiroNumero = 5;
+const segundoNumero = '10';
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 10;
+console.log(resultadoTriplicado)
+
+// Vendo o tipo de dado
+
+ console.log(typeof primeiroNumero);
+ console.log(typeof segundoNumero);
+ 
