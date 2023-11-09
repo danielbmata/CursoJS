bd0ad80e7b4834b5cba71b9925b809662e156783
Daniel <d7mata@gmail.com>
2023-11-09 08:21:10 -0300

    Update index.js

--- aula6_variaveis/index.js
+++ aula6_variaveis/index.js
@@ -5,3 +5,9 @@ console.log('Em 2000', nome, 'conheceu Maria.');
 console.log(nome, 'casou-se com Maria em 2012.');
 console.log('Maria teve um filho com', nome, 'em 2015.');
 console.log('O filho de', nome, 'se chama Eduardo');
+
+// Posso declarar uma var e inicia-la posteriormente
+
+let nome2;  // Declarei a var
+nome2 = 'Qualquer valor'  // Iniciei a var
+console.log(nome2)
