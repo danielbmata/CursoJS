ded564ca79bb2409e961100060363325adc78603
Daniel <d7mata@gmail.com>
2023-11-08 21:17:34 -0300

    Atualização CursoJS

--- aula5_html+js/index.js
+++ aula5_html+js/index.js
@@ -6,3 +6,10 @@ node "/Users/danielmata/Desktop/CursoJS/aula5_html+js/index.js"
 */
 
 console.log("Um texto qualquer");
+
+// Um comentario
+console.log("Olá mundooo");
+console.log("Este trecho será exibido no console do navegador");
+
+// o exemplo do alert que não abre no Node, somente no navegador
+alert("Ola mundo");
