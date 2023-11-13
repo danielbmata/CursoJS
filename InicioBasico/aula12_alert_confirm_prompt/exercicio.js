e0b84f45da0c4b465363a1bec03fdc1645ce3537
Daniel <d7mata@gmail.com>
2023-11-13 10:37:27 -0300

    update

--- InicioBasico/aula12_alert_confirm_prompt/exercicio.js
+++ InicioBasico/aula12_alert_confirm_prompt/exercicio.js
@@ -3,8 +3,11 @@
 alert('Bem vindo');
 alert('Digite um número em cada caixa, para obter a soma.');
 let num1 = prompt('Digite o primeiro número');
+console.log(num1)
 let num2 = prompt('Digite o segundo número');
+console.log(num2)
 num1 = Number(num1)
 num2 = Number(num2)
-let soma = num1 + num2;
+const soma = num1 + num2;
+console.log(soma)
 alert(`A soma é ${soma}`)
---
+++ InicioBasico/aula13_exercicio_variaveis/exercicio.js
@@ -0,0 +1,17 @@
+// A tem que ter o valor de B
+// B tem que ter o valor de C
+// C tem que ter o valor de A
+
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A 
+
+
+
+
+
+
+
+
+
+console.log(varA, varB, varC);
